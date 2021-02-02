519757da9c3c967ccac5ad0178874d365a8e6b53
jason_pengel <jasonpengel@gmail.com>
2021-02-02 21:45:27 +0100

    Api connection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using PokemonGame.Models;
+using PokemonGame.Pokemon;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace PokemonGame.Controllers
@@ -12,15 +16,86 @@ namespace PokemonGame.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        PokemonClient pokemonClient;
         public HomeController(ILogger<HomeController> logger)
         {
+            pokemonClient = new PokemonClient();
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index1()
         {
-            return View();
+            Pokemon.Pokemon pokemon = await PokemonClient.GetPokemonAsync("pokemon/mankey");
+            return View(pokemon);
+        }
+
+        public async Task<IActionResult> ChooseStarter()
+        {
+            //Pokemon.Pokemon pokemon = await PokemonClient.GetPokemonAsync("pokemon/mankey");
+            List<Pokemon.Pokemon> pokemons = new List<Pokemon.Pokemon>();
+            Pokemon.Pokemon pokemon1 = await PokemonClient.GetStarterPokemon("pokemon/charmander");
+            Pokemon.Pokemon pokemon2 = await PokemonClient.GetStarterPokemon("pokemon/squirtle");
+            Pokemon.Pokemon pokemon3 = await PokemonClient.GetStarterPokemon("pokemon/bulbasaur");
+            pokemons.Add(pokemon1);
+            pokemons.Add(pokemon2);
+            pokemons.Add(pokemon3);
+
+            return View(pokemons);
+        }
+
+        public async Task<IActionResult> GameScreen(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                //do something with http client
+                client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+            }
+            Pokemon.Pokemon pokemon = await PokemonClient.GetPokemonAsync("pokemon/" + id.ToString());
+
+            int randomIdForPokemon;
+            Random rand = new Random();
+            randomIdForPokemon = rand.Next(1, 400);
+
+            using (var client = new HttpClient())
+            {
+                //do something with http client //9.png
+                client.BaseAddress = new Uri("https://pokeres.bastionbot.org/images/pokemon/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var img = "";
+
+                HttpResponseMessage response = await client.GetAsync(id.ToString()+".png");
+                if (response.IsSuccessStatusCode)
+                {
+                    img = await response.Content.ReadAsStringAsync();
+                    pokemon.Image = img;
+                }
+
+            }
+
+            await PokemonClient.GetRandomPokemonAsync(randomIdForPokemon, pokemon);
+            return View(pokemon);
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            //Pokemon.Pokemon pokemon = await PokemonClient.GetPokemonAsync("pokemon/mankey");
+            List<Pokemon.Pokemon> pokemons = new List<Pokemon.Pokemon>();
+            Pokemon.Pokemon pokemon1 = await PokemonClient.GetStarterPokemon("pokemon/charmander");
+            Pokemon.Pokemon pokemon2 = await PokemonClient.GetStarterPokemon("pokemon/squirtle");
+            Pokemon.Pokemon pokemon3 = await PokemonClient.GetStarterPokemon("pokemon/bulbasaur");
+            pokemons.Add(pokemon1);
+            pokemons.Add(pokemon2);
+            pokemons.Add(pokemon3);
+
+            return View(pokemons);
         }
 
         public IActionResult Privacy()
---
+++ Pokemon/PokemonClient.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace PokemonGame.Pokemon
+{
+    public class PokemonClient
+    {
+        static HttpClient client;
+        static Pokemon Pmon = new Pokemon();
+        public PokemonClient()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public static async Task<Pokemon> GetPokemonAsync(string path)
+        {
+            var pokemon = "";
+
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                pokemon = await response.Content.ReadAsStringAsync();
+                var yourObject = JsonConvert.DeserializeObject<Pokemon>(pokemon);
+                Pmon = Pokemon.FromJson(pokemon);
+                Console.WriteLine(Pmon.Name + " " + Pmon.Moves[0].MoveMove.Name);
+                return yourObject;
+            }
+            return null;
+        }
+
+        //dirty parameter
+        public static async Task GetRandomPokemonAsync(int id,Pokemon Trainer)
+        {
+            var pokemon = "";
+
+            HttpResponseMessage response = await client.GetAsync("pokemon/"+id.ToString());
+            if (response.IsSuccessStatusCode)
+            {
+                pokemon = await response.Content.ReadAsStringAsync();
+                var yourObject = JsonConvert.DeserializeObject<Pokemon>(pokemon);
+                Trainer.RandomPokemon = yourObject;
+            }
+            
+        }
+
+        public static async Task<Pokemon> GetStarterPokemon(string path)
+        {
+            var pokemon = "";
+
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                pokemon = await response.Content.ReadAsStringAsync();
+                var yourObject = JsonConvert.DeserializeObject<Pokemon>(pokemon);
+                return yourObject;
+            }
+            return null;
+        }
+
+        public static async Task<Ability> GetAbilitiesAsync(string path, int id)
+        {
+            Ability abilities = null;
+
+            HttpResponseMessage response = await client.GetAsync(path + "/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                abilities = await response.Content.ReadAsAsync<Ability>();
+            }
+            return abilities;
+        }
+
+        public static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            //await GetAbilitiesAsync("ability", 2);
+            await GetPokemonAsync("pokemon/ditto");
+        }
+
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Hello World!");
+
+        //    RunAsync().GetAwaiter().GetResult();
+
+        //    Console.ReadKey();
+        //}
+    }
+}
+
